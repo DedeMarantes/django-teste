ac628c77e0a6f7b630777950a434d1855e334316
DedeMarantes <andre.marantes@gmail.com>
2023-01-25 19:13:50 -0300

    Adicionando novo modelo e alterações

--- escola/views.py
+++ escola/views.py
@@ -1,12 +1,22 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import (AlunoSerializer, CursoSerializer,
+                               MatriculaSerializer)
+
 
 # Create your views here.
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {
-            'id': 1,
-            'nome': 'Andre'
-        }
-        return JsonResponse(aluno)
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import include, path
+from rest_framework import routers
+
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos',CursosViewSet, basename='Cursos')
+router.register('matriculas',MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
+    path('', include(router.urls) )
 ]
