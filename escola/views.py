c7d211769c00de72d1e100433f4ba148be76aea9
DedeMarantes <andre.marantes@gmail.com>
2023-01-25 15:58:44 -0300

    Adicionando modelos e serializer

---
+++ escola/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def alunos(request):
+    if request.method == 'GET':
+        aluno = {
+            'id': 1,
+            'nome': 'Andre'
+        }
+        return JsonResponse(aluno)
